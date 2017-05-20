173b3c665e9b5bd49ece39755407d608a08c2dd6
Norbinarius <norbinarius@list.ru>
2017-05-20 19:58:47 +0300

    Policies, paginate.

---
+++ app/Policies/MainPolicy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Specifics;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class MainPolicy
+{
+    use HandlesAuthorization;
+
+    public function action(User $user)
+    {
+        return $user->email == 'admin@example.com';
+    }
+}
